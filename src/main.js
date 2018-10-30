71ad85d48965a926e4a7f3c35a7ac2f9c105e4bf
fairysd <fairysd@live.cn>
2018-10-31 02:18:38 +0800

    footer 安定

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
+import echarts from 'echarts'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
+
+Vue.prototype.$echarts = echarts
 
 new Vue({
   router,
--- src/views/CaseManage.vue
+++ src/views/CaseManage.vue
@@ -17,7 +17,7 @@
           v-model="searchValue">
         </el-input>
       </el-col>
-      <el-col :span="3" :offset="1" class="create-btn"><button>创建新案件</button></el-col>
+      <el-col :span="3" :offset="1" class="create-btn"><button @click="dialogVisible = true">创建新案件</button></el-col>
     </el-row>
     <el-row  class="content-head">
       <el-col :span="20" :offset="2">
@@ -63,8 +63,44 @@
       <el-col :span="24" class="divider"></el-col>
     </el-row>
     <el-row class="footer"> 
-      <el-col :span="24" class=""></el-col>
+        <el-col :span="12">
+          <div id="caseLine" class="line-echart" :style="'height:300px;width:98%;'"></div>
+        </el-col>
+        <el-col :span="12">
+          <div id="casePie" class="pie-echart" :style="'height:300px;width:98%;'"></div>
+        </el-col>
     </el-row>
+    <el-dialog
+      title="创建新案件"
+      width="80%"
+      :visible.sync="dialogVisible"
+      class="new-case">
+      <el-row class="new-form">
+        <el-col :span="8"><label for="">委托人：</label><input type="text"><span>*</span></el-col>
+        <el-col :span="8"><label for="">联系方式：</label><input type="text"><span>*</span></el-col>
+        <el-col :span="8"><label for="">案由：</label><input type="text"><span>*</span></el-col>
+        </el-row>
+        <el-row class="new-form">
+        <el-col :span="8"><label for="">受理法院：</label><input type="text"><span>*</span></el-col>
+        <el-col :span="8"><label for="">案件类型：</label><input type="text"><span>*</span></el-col>
+        <el-col :span="8"><label for="">诉讼地位：</label><input type="text"><span>*</span></el-col>
+      </el-row>
+       <el-row class="new-form from3">
+        <el-col :span="24"><label for="">相对方：</label><input type="text"><input type="text"><input type="text"><button>添加</button><div class="divider"></div></el-col>
+      </el-row>
+       <el-row class="new-form from4">
+        <el-col :span="8"><label for="">承办方：</label><input type="text"><span></span></el-col>
+        <el-col :span="8"><label for="">书记员：</label><input type="text"><span></span></el-col>
+        <el-col :span="8"><label for="">对方代理 ：</label><input type="text"><span></span></el-col>
+      </el-row>
+       <el-row class="new-form from5">
+        <el-col :span="8"><label for="">律师费：</label><input type="text"><span></span></el-col>
+        <el-col :span="8"><label for="">标的：</label><input type="text"><span></span></el-col>
+      </el-row>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -72,7 +108,136 @@
 
 export default {
   name: "caseManage",
-  components: {}
+  components: {},
+  data() {
+    return {
+      searchValue: "",
+      dialogVisible: false
+    };
+  },
+  mounted() {
+    let lineChart = this.$echarts.init(document.getElementById("caseLine"));
+    let option_line = {
+      xAxis: {
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: "line"
+        }
+      ]
+    };
+    lineChart.setOption(option_line);
+    //
+    let pieChart = this.$echarts.init(document.getElementById("casePie"));
+    let option_pie = {
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+      },
+      legend: {
+        orient: "vertical",
+        x: "left",
+        data: [
+          "直达",
+          "营销广告",
+          "搜索引擎",
+          "邮件营销",
+          "联盟广告",
+          "视频广告",
+          "百度",
+          "谷歌",
+          "必应",
+          "其他"
+        ]
+      },
+      series: [
+        {
+          name: "访问来源",
+          type: "pie",
+          selectedMode: "single",
+          radius: [0, "30%"],
+
+          label: {
+            normal: {
+              position: "inner"
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          }
+        },
+        {
+          name: "访问来源",
+          type: "pie",
+          radius: ["40%", "55%"],
+          label: {
+            normal: {
+              formatter: "{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ",
+              backgroundColor: "#eee",
+              borderColor: "#aaa",
+              borderWidth: 1,
+              borderRadius: 4,
+              // shadowBlur:3,
+              // shadowOffsetX: 2,
+              // shadowOffsetY: 2,
+              // shadowColor: '#999',
+              // padding: [0, 7],
+              rich: {
+                a: {
+                  color: "#999",
+                  lineHeight: 22,
+                  align: "center"
+                },
+                // abg: {
+                //     backgroundColor: '#333',
+                //     width: '100%',
+                //     align: 'right',
+                //     height: 22,
+                //     borderRadius: [4, 4, 0, 0]
+                // },
+                hr: {
+                  borderColor: "#aaa",
+                  width: "100%",
+                  borderWidth: 0.5,
+                  height: 0
+                },
+                b: {
+                  fontSize: 16,
+                  lineHeight: 33
+                },
+                per: {
+                  color: "#eee",
+                  backgroundColor: "#334455",
+                  padding: [2, 4],
+                  borderRadius: 2
+                }
+              }
+            }
+          },
+          data: [
+            { value: 335, name: "直达" },
+            { value: 310, name: "邮件营销" },
+            { value: 234, name: "联盟广告" },
+            { value: 135, name: "视频广告" },
+            { value: 1048, name: "百度" },
+            { value: 251, name: "谷歌" },
+            { value: 147, name: "必应" },
+            { value: 102, name: "其他" }
+          ]
+        }
+      ]
+    };
+    pieChart.setOption(option_pie);
+  },
+  methods: {}
 };
 </script>
 <style lang="less">
@@ -128,87 +293,177 @@ export default {
       color: #ffffff;
     }
   }
-  .content-head{
-    ul{
+  .content-head {
+    ul {
       padding: 20px 0;
       margin: 0;
       border-bottom: 1px solid #eee;
-      li{
+      li {
         list-style: none;
         display: inline-block;
         text-align: center;
         font-size: 24px;
-        color:#626262;
+        color: #626262;
       }
-      .first{
+      .first {
         width: 16%;
       }
-      .second{
+      .second {
         width: 18%;
       }
-      .third{
+      .third {
         width: 18%;
       }
-      .fouth{
-        width:32%;
+      .fouth {
+        width: 32%;
       }
-      .fifth{
-        width:16%;
+      .fifth {
+        width: 16%;
       }
     }
   }
-    .content-body{
-    ul.content-body{
+  .content-body {
+    ul.content-body {
       padding: 20px 0;
       margin: 0;
       border-bottom: 1px solid #eee;
-      li{
+      li {
         list-style: none;
         display: inline-block;
         text-align: center;
         font-size: 20px;
-        color:#838485;
+        color: #838485;
       }
-      .first{
+      .first {
         width: 16%;
       }
-      .second{
+      .second {
         width: 18%;
       }
-      .third{
+      .third {
         width: 18%;
       }
-      .fouth{
-        width:32%;
+      .fouth {
+        width: 32%;
         text-align: left;
-        span{
-          background-color: #2CA7E7;
+        span {
+          background-color: #2ca7e7;
           border-radius: 4px;
           color: #fff;
           padding: 3px 10px;
         }
-        
       }
-      .fifth{
-        width:16%;
+      .fifth {
+        width: 16%;
       }
     }
-    .el-pagination{
+    .el-pagination {
       text-align: center;
       padding: 20px 0;
     }
   }
-  .divider{
+  .divider {
     height: 34px;
     background-color: #eee;
   }
-  .footer{
+  .footer {
     height: 300px;
-     background: linear-gradient(
+    background: linear-gradient(to right, rgb(40, 217, 206), rgb(51, 110, 206));
+  }
+  .new-case {
+    .el-dialog {
+      border-radius: 20px;
+    }
+    .el-dialog__header {
+      background-color: #f8f7fa;
+      padding: 30px 0 30px 43px;
+      border-top-left-radius: 20px;
+      border-top-right-radius: 20px;
+      .el-dialog__title {
+        font-size: 30px;
+        color: #626262;
+      }
+      .el-dialog__headerbtn {
+        font-size: 30px;
+        top: 30px;
+      }
+    }
+    .new-form {
+      text-align: right;
+      margin-bottom: 30px;
+      label {
+        font-size: 24px;
+        color: #626262;
+        line-height: 50px;
+      }
+      input {
+        height: 50px;
+        box-sizing: border-box;
+        vertical-align: top;
+        width: 60%;
+        border: 1px solid #eee;
+        background-color: #f7f7fa;
+        border-radius: 10px;
+        font-size: 24px;
+      }
+      input:focus {
+        outline: none;
+        border-color: #409eff;
+      }
+      span {
+        font-size: 24px;
+        margin-left: 10px;
+        color: red;
+      }
+    }
+    .from3{    
+      .divider{
+        display: inline-block;
+        width: 7%;
+        height: 0;
+      }  
+      input{
+        display: inline-block;
+        width: 23%;
+        margin-right: 10px;
+      }
+      label{
+        vertical-align: top;
+      }
+      button {
+        width:10%;
+        height: 50px;
+        border: none;
+        border-radius: 5px;
+        display: inline-block;
+        vertical-align: top;
+        color: #fff;
+        font-size: 24px;
+      background: linear-gradient(
         to right,
         rgb(40, 217, 206),
         rgb(51, 110, 206)
       );
+      }
+      padding-right: 0;
+    }
+    .from4,.from5{
+      span{
+        width:10px;
+        display: inline-block;
+      }
+    }
+    .el-dialog__footer{
+      text-align: center;
+      padding-bottom: 70px;
+    }
+    .dialog-footer{
+      text-align: center;
+      button{
+        width: 40%;
+         background: linear-gradient(to right, rgb(40, 217, 206), rgb(51, 110, 206));
+      }
+    }
   }
 }
 </style>

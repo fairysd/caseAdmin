fc3a0097ce1c812b743d2573b975082dc2ab6735
fairysd <fairysd@live.cn>
2018-11-05 03:44:28 +0800

    bigdata,book

--- src/views/BigData.vue
+++ src/views/BigData.vue
@@ -1,7 +1,73 @@
 <template>
-  <div class="big-data">
-    <el-row>
-      大数据
+  <div id="bigData" class="big-data">
+     <el-row class="body-head">      
+      <el-col :span="8" :offset="8" class="serach"> 
+        <el-input
+          placeholder="搜索"
+          suffix-icon="el-icon-search"
+          class="search-box"
+          v-model="searchValue">
+        </el-input>
+      </el-col>
+    </el-row>
+    <el-row class="content-body">      
+      <el-col :span="24" class="data"> 
+        <h4>案例分析</h4>
+       <ul>
+         <li>
+           <img src="../assets/images/bigdata.png" alt="">
+           <p>法律法规</p>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+        </ul>
+      </el-col>
+      <el-col :span="24" class="data"> 
+        <h4>法律关系</h4>
+       <ul>
+         <li>
+           <img src="../assets/images/bigdata.png" alt="">
+           <p>法律法规</p>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+        </ul>
+      </el-col>
+      <el-col :span="24" class="data"> 
+        <h4>法律维护</h4>
+       <ul>
+         <li>
+           <img src="../assets/images/bigdata.png" alt="">
+           <p>法律法规</p>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+         <li>
+         </li>
+        </ul>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -10,8 +76,64 @@
 
 export default {
   name: "bigData",
-  components: {
-    
+  components: {},
+  data() {
+    return {
+      searchValue: ""
+    };
+  },
+  mounted(){
+    this.openNotice();
+  },
+  methods:{
+     openNotice() {
+        this.$notify({
+          title: '提示',
+          message: '勾选你想查询的网站，点击搜索按钮',
+          duration: 0
+        });
+      }
   }
 };
 </script>
+<style lang="less">
+#bigData {
+  background-color: #f6f6f8;
+  .body-head {
+    padding-top: 50px;
+    padding-bottom: 100px;
+  }
+  .content-body{
+    padding-bottom: 150px;
+    h4{
+      padding-left: 5%;
+      color: #626262;
+      font-size: 24px;
+      font-weight: normal;
+    }
+    ul{
+      padding: 0 5%;
+      li{
+        list-style: none;
+        display: inline-block;
+        background-color: #fff;
+        height: 200px;
+        width: 15%;
+        margin-right: 1%;
+        vertical-align: top;
+        text-align: center;
+        img{
+          margin-top: 14%;
+        }
+        p{
+          text-align: center;
+          color: #626262;
+        }
+      }
+      li:nth-child(6){
+        margin-right: 0;
+      }
+    }
+  }
+}
+</style>
--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,7 +1,42 @@
 <template>
-  <div class="book">
-    <el-row>
-      通讯录
+  <div id="book" class="book">
+    <el-row class="body-head">      
+      <el-col :span="8" :offset="8" class="serach"> 
+        <el-input
+          placeholder="搜索"
+          suffix-icon="el-icon-search"
+          class="search-box"
+          v-model="searchValue">
+        </el-input>
+      </el-col>
+    </el-row>
+    <el-row class="content-head">      
+      <el-col :span="18"  class="list"> 
+        <ul>
+          <li>
+            <img src="../assets/images/book.png" alt="">
+            <p>江苏省高级人民法院</p>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </el-col>
+      <el-col :span="6"  class="list-history"> 
+        <h4>历史纪录</h4>
+       <ul>
+         <li>江苏省人民法院法官信息</li>
+         <li>江苏省人民法院法官信息</li>
+         <li>江苏省人民法院法官信息</li>
+         <li>江苏省人民法院法官信息</li>
+         <li>江苏省人民法院法官信息</li>
+       </ul>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -12,6 +47,77 @@ export default {
   name: "book",
   components: {
     
+  },
+  data(){
+    return{
+      searchValue:""
+    }
   }
 };
 </script>
+<style lang="less">
+#book {
+  background-color: #f6f6f8;
+  .body-head {
+    padding-top: 50px;
+    padding-bottom: 35px;
+  }
+  .content-head{
+    .list{
+      padding-left: 5%;
+      ul{
+        padding: 0;
+        margin: 0;
+        li{
+          list-style: none;
+          display: inline-block;
+          width: 30%;
+          margin-right: 3%;
+          margin-bottom: 20px;
+          height: 150px;;
+          background-color: #fff;
+          vertical-align: top;
+          text-align: center;
+          img{
+            width: 100%;
+            height: 55%;
+          }
+          p{
+            font-size: 24px;
+          }
+        }
+      }
+    }
+    .list-history{
+      padding-right: 5%;
+      h4{
+        font-size: 24px;
+        color:#626262;
+        background-color: #E8EAEC;
+        padding: 30px;
+        margin: 0;
+        border-top-right-radius: 10px;
+        border-top-left-radius: 10px;
+        border: 1px solid #000;
+        border-bottom: none;
+      }
+      ul{
+        padding: 0;
+        margin: 0;
+        border: 1px solid #000;
+        border-top: none;
+        border-bottom-right-radius: 10px;
+        border-bottom-left-radius: 10px;
+        li{
+          list-style: none;
+          padding:  20px 0; 
+          padding-left: 20px;
+          background-color: #fff;
+          border-bottom-right-radius: 10px;
+        border-bottom-left-radius: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
